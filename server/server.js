0a34e4178ff9e1a0464b307eac81de8111e57851
arashma <alidadi_arash@yahoo.com>
2018-12-28 16:14:47 +0330

    Great new user,and alert others

--- server/server.js
+++ server/server.js
@@ -17,9 +17,18 @@ io.on('connection' , (socket)=>{
   socket.on('disconnect',()=>{
     console.log("User was disconnected");
 });
- 
 
+socket.emit('newMessage',{
+    from:'Admin',
+    text:'Wellcome to chat app',
+    createdAt: new Date().getTime()
+});
 
+socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New user joined',
+    createdAt: new Date().getTime()
+});
 socket.on('createMessage',(message)=>{
     console.log("Created message",message);
     io.emit('newMessage',{
@@ -27,12 +36,27 @@ socket.on('createMessage',(message)=>{
         text:message.text,
         createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage',{
+    //     from:message.from,
+    //     text:message.text,
+    //     createdAt: new Date().getTime()
+    // });
 })
 
 });
 
-
-
 server.listen(port,()=>{
     console.log(`Server is up on port ${port}`);
 })
+
+
+
+
+
+
+
+
+
+
+
